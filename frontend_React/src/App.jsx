7723818421f4d6bc7f34d8522708e48e35b9f071
Vedant Zope <vedantzope9@gmail.com>
2025-01-17 23:43:32 +0530

    Frontend Setup

---
+++ frontend_React/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='App'>
+      <header className='bg-primary text-white text-center py-4'>
+        <h1>ChatBot</h1>
+      </header>
+      {/*Chat Input */}
+      {/* Response*/}
+    </div>
+  )
+}
+
+export default App
